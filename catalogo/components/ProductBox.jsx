25eccb5fcd43ab0c1158317b3180ba67e4a93cf7
Paula Cano <paulacanoroman@gmail.com>
2023-10-10 13:44:43 +0200

    estilos tarjeta de cada producto

--- catalogo/components/ProductBox.jsx
+++ catalogo/components/ProductBox.jsx
@@ -6,9 +6,12 @@ const ProductBox = ({actualProduct}) => {
     
   return (
     <div className={styleCSS.productBox}>
-        <h3>{actualProduct.title}</h3>
-        <img src={actualProduct.image} className={styleCSS.productBox_img}></img>
-        <h2>{actualProduct.price}</h2>
+        {/* <h3>{actualProduct.title}</h3> */}
+        {/* <img src={actualProduct.image} className={styleCSS.productBox_img}></img>
+        <h2>{actualProduct.price}</h2> */}
+        <div className={styleCSS.productBox_title} ></div>
+        <div className={styleCSS.productBox_img}></div>
+        <div className={styleCSS.productBox_price}></div>
     </div>
   )
 }
